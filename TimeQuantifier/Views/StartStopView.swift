b519e6e21323a43fc93a98f7a90d4ba79293655a
baltoli <bsc28@cam.ac.uk>
2016-10-12 15:35:25 +0100

    Add views and basic storyboard

---
+++ TimeQuantifier/Views/StartStopView.swift
@@ -0,0 +1,13 @@
+//
+//  StartStopView.swift
+//  TimeQuantifier
+//
+//  Created by Bruce Collie on 12/10/2016.
+//  Copyright © 2016 Bruce Collie. All rights reserved.
+//
+
+import UIKit
+
+class StartStopView : UIView {
+    
+}
